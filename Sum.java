ff4c1895bc9e038a4aa6e4e20a9e036db27be062
Akhil Nair <akhil.202122@gmail.com>
2023-01-04 08:28:02 +0530

    second commit

--- Sum.java
+++ Sum.java
@@ -3,7 +3,8 @@ public class Sum {
     public static void main (String[] args) {
     int a = 5;
     int b = 5;
+    int c = 5;
 
-    system.out.println(a+b);
+    system.out.println(a+b+c);
     }
 }
